daa7d0cfe80fe9bb71318858caf9110910added2
Farukh <cool.faruh42@gmail.com>
2020-08-05 12:14:33 +0600

    calculator added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,26 @@ import io.javalin.Javalin;
 public class Main {
     public static void main(String[] args) {
         Javalin app = Javalin.create();
-        app.get("/helloworldpage", context -> {
-            context.result("Hello, world!");
-        });
-        app.get("/:", context -> {
-            context.result("Hello, world!");
+        app.get("/:firtvariable/:action':secondvariable", context -> {
+            context.result("Answer is " + context.pathParam(calculate(Integer.parseInt("firstvariable"), Integer.parseInt("secondvariable"), "action")));
         });
         app.start(8080);
+    }
+
+    public static String calculate(int firstvariable, int secondvariable, String action) {
+        String answer = "Error";
+        if (action.equals("+")) {
+            answer = new Integer(firstvariable + secondvariable).toString();
+        }
+        if (action.equals("-")) {
+            answer = new Integer(firstvariable - secondvariable).toString();
+        }
+        if (action.equals("*")) {
+            answer = new Integer(firstvariable * secondvariable).toString();
+        }
+        if (action.equals(":")){
+            answer = new Integer (firstvariable / secondvariable).toString();
+        }
+        return answer;
     }
 }
