53b7df41a30333bbe27a0e2f02a0930d3a2f1380
Farukh <cool.faruh42@gmail.com>
2020-08-10 10:06:53 +0600

    calculator 2.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,25 +3,28 @@ import io.javalin.Javalin;
 public class Main {
     public static void main(String[] args) {
         Javalin app = Javalin.create();
-        app.get("/:firtvariable/:action':secondvariable", context -> {
-            context.result("Answer is " + context.pathParam(calculate(Integer.parseInt("firstvariable"), Integer.parseInt("secondvariable"), "action")));
+        app.get("/:firtvariable/:action/:secondvariable", context -> {
+            context.result("Answer is " + calculate(Integer.parseInt(context.pathParam("firstvariable")), Integer.parseInt(context.pathParam("secondvariable")), context.pathParam("action")));
+        });
+        app.get("/JuiceEye", context -> {
+            context.result("Hello, JuiceEye");
         });
         app.start(8080);
     }
 
-    public static String calculate(int firstvariable, int secondvariable, String action) {
-        String answer = "Error";
+    public static String calculate(int firstvariable, int secondVariable, String action) {
+        String answer = "isn't defined. Invalid action";
         if (action.equals("+")) {
-            answer = new Integer(firstvariable + secondvariable).toString();
+            answer = Integer.toString(firstvariable + secondVariable);
         }
         if (action.equals("-")) {
-            answer = new Integer(firstvariable - secondvariable).toString();
+            answer = Integer.toString(firstvariable - secondVariable);
         }
         if (action.equals("*")) {
-            answer = new Integer(firstvariable * secondvariable).toString();
+            answer = Integer.toString(firstvariable * secondVariable);
         }
         if (action.equals(":")){
-            answer = new Integer (firstvariable / secondvariable).toString();
+            answer = Integer.toString(firstvariable / secondVariable);
         }
         return answer;
     }
